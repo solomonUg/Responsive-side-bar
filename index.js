9197dc2e7747d808abca523c121bcc66018ce3fc
SolomonUg <solomonchris700@gmail.com>
2024-09-21 09:54:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const sideBar = document.querySelector(".sidebar")
+function showSideBar() {
+sideBar.style.display = "flex";
+}
+
+function hideSideBar() {
+    sideBar.style.display = "none";
+}
